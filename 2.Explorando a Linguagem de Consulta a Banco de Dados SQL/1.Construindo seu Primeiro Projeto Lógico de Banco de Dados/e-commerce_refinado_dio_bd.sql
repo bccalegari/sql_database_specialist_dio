9a25551b3487d026fbe6b81ffb3f84f2b8fb9106
Bruno da Costa Calegari <109561086+bccalegari@users.noreply.github.com>
2023-01-08 21:58:02 -0300

    v1

--- 2.Explorando a Linguagem de Consulta a Banco de Dados SQL/1.Construindo seu Primeiro Projeto Lógico de Banco de Dados/e-commerce_refinado_dio_bd.sql
+++ 2.Explorando a Linguagem de Consulta a Banco de Dados SQL/1.Construindo seu Primeiro Projeto Lógico de Banco de Dados/e-commerce_refinado_dio_bd.sql
@@ -8,44 +8,44 @@ USE e_commerce;
 CREATE TABLE conta_pf(
 	id_conta CHAR(4) NOT NULL,
 	nome_usuario VARCHAR(25) NOT NULL,
-    senha VARCHAR(32) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    	senha VARCHAR(32) NOT NULL,
+    	email VARCHAR(255) NOT NULL,
 	cpf CHAR(11) NOT NULL,
 	CONSTRAINT UNIQUE_contapf_nome_usuario UNIQUE (nome_usuario),
-    CONSTRAINT UNIQUE_contapf_cpf UNIQUE (cpf),
-    PRIMARY KEY (id_conta)
+    	CONSTRAINT UNIQUE_contapf_cpf UNIQUE (cpf),
+    	PRIMARY KEY (id_conta)
 	);
 
 CREATE TABLE conta_pj(
 	id_conta CHAR(4) NOT NULL,
 	nome_usuario VARCHAR(25) NOT NULL,
-    senha VARCHAR(32) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    	senha VARCHAR(32) NOT NULL,
+    	email VARCHAR(255) NOT NULL,
 	cnpj CHAR(14) NOT NULL,
 	CONSTRAINT UNIQUE_contapj_nome_usuario UNIQUE (nome_usuario),
-    CONSTRAINT UNIQUE_contapj_cnpj UNIQUE (cnpj),
-    PRIMARY KEY (id_conta)
+    	CONSTRAINT UNIQUE_contapj_cnpj UNIQUE (cnpj),
+    	PRIMARY KEY (id_conta)
 	);
 
 CREATE TABLE cliente(
 	id_cliente INT NOT NULL AUTO_INCREMENT,
 	nome VARCHAR(15) NOT NULL,
-    sobrenome VARCHAR(20) NOT NULL,
-    sexo ENUM('M', 'F') NOT NULL,
-    data_nascimento DATE NOT NULL,
+    	sobrenome VARCHAR(20) NOT NULL,
+    	sexo ENUM('M', 'F') NOT NULL,
+    	data_nascimento DATE NOT NULL,
 	telefone VARCHAR(10) NOT NULL,
-    endereco VARCHAR(100) NOT NULL,
-    id_conta CHAR(4) NOT NULL,
+    	endereco VARCHAR(100) NOT NULL,
+    	id_conta CHAR(4) NOT NULL,
 	CONSTRAINT UNIQUE_cliente_id_conta UNIQUE (id_conta),
 	CONSTRAINT FK_cliente_conta_pf FOREIGN KEY (id_conta) REFERENCES conta_pf(id_conta) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT FK_cliente_conta_pj FOREIGN KEY (id_conta) REFERENCES conta_pj(id_conta) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (id_cliente)
+    	PRIMARY KEY (id_cliente)
 	);
 
 CREATE TABLE pagamento(
 	id_pagamento INT NOT NULL AUTO_INCREMENT,
-    tipo ENUM('credito', 'debito', 'pix', 'boleto', 'transferencia bancaria') NOT NULL,
-    id_cliente INT NOT NULL,
+    	tipo ENUM('credito', 'debito', 'pix', 'boleto', 'transferencia bancaria') NOT NULL,
+    	id_cliente INT NOT NULL,
 	CONSTRAINT FK_pagamento_id_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente) ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY (id_pagamento)
 );
@@ -53,18 +53,18 @@ CREATE TABLE pagamento(
 CREATE TABLE entrega(
 	id_entrega INT NOT NULL AUTO_INCREMENT,
 	valor_frete FLOAT NOT NULL,
-    transportadora VARCHAR(45) NOT NULL,
+    	transportadora VARCHAR(45) NOT NULL,
 	codigo_rastreio VARCHAR(45),
 	PRIMARY KEY (id_entrega)
 );
 
 CREATE TABLE pedido(
 	id_pedido INT NOT NULL AUTO_INCREMENT,
-    status_pedido ENUM('Pedido recebido', 'Pagamento aprovado', 'Em separação', 'Enviado à transportadora', 'Pedido saiu para entrega', 'Pedido entregue') NOT NULL,
+    	status_pedido ENUM('Pedido recebido', 'Pagamento aprovado', 'Em separação', 'Enviado à transportadora', 'Pedido saiu para entrega', 'Pedido entregue') NOT NULL,
 	observacao VARCHAR(100),
 	id_cliente INT NOT NULL,
-    id_pagamento INT,
-    id_entrega INT,
+    	id_pagamento INT,
+    	id_entrega INT,
 	CONSTRAINT UNIQUE_pedido_id_entrega UNIQUE (id_entrega),
 	CONSTRAINT FK_pedido_id_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT FK_pedido_pagamento FOREIGN KEY (id_pagamento) REFERENCES pagamento(id_pagamento) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -74,9 +74,9 @@ CREATE TABLE pedido(
 
 CREATE TABLE produto(
 	id_produto INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(45) NOT NULL,
-    categoria VARCHAR(20) NOT NULL,
-    descricao VARCHAR(100) NOT NULL,
+    	nome VARCHAR(45) NOT NULL,
+    	categoria VARCHAR(20) NOT NULL,
+    	descricao VARCHAR(100) NOT NULL,
 	valor FLOAT NOT NULL,
 	PRIMARY KEY (id_produto)
 );
@@ -92,12 +92,12 @@ CREATE TABLE relacao_pedido_produto(
 
 CREATE TABLE fornecedor(
 	id_fornecedor INT NOT NULL AUTO_INCREMENT,
-    razao_social VARCHAR(80) NOT NULL,
+    	razao_social VARCHAR(80) NOT NULL,
 	cnpj CHAR(14) NOT NULL,
-    endereco VARCHAR(100) NOT NULL,
-    telefone VARCHAR(10) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    CONSTRAINT UNIQUE_fornecedor_cnpj UNIQUE(cnpj),
+    	endereco VARCHAR(100) NOT NULL,
+    	telefone VARCHAR(10) NOT NULL,
+    	email VARCHAR(255) NOT NULL,
+    	CONSTRAINT UNIQUE_fornecedor_cnpj UNIQUE(cnpj),
 	PRIMARY KEY (id_fornecedor)
 );
 
@@ -111,18 +111,18 @@ CREATE TABLE fornecedor_disponibiliza_produto(
 
 CREATE TABLE estoque(
 	id_estoque INT NOT NULL AUTO_INCREMENT,
-    endereco VARCHAR(100) NOT NULL,
-    responsavel VARCHAR(50) NOT NULL,
-    telefone_responsavel VARCHAR(10) NOT NULL,
-    email_responsavel VARCHAR(255) NOT NULL,
+    	endereco VARCHAR(100) NOT NULL,
+    	responsavel VARCHAR(50) NOT NULL,
+    	telefone_responsavel VARCHAR(10) NOT NULL,
+    	email_responsavel VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id_estoque)
 );
 
 CREATE TABLE produto_no_estoque(
 	id_estoque INT NOT NULL,
-    id_produto INT NOT NULL,
-    quantidade INT NOT NULL,
-    CONSTRAINT FK_produto_no_estoque_id_estoque FOREIGN KEY (id_estoque) REFERENCES estoque(id_estoque) ON DELETE CASCADE ON UPDATE CASCADE,
+    	id_produto INT NOT NULL,
+    	quantidade INT NOT NULL,
+    	CONSTRAINT FK_produto_no_estoque_id_estoque FOREIGN KEY (id_estoque) REFERENCES estoque(id_estoque) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT FK_produto_no_estoque_id_produto FOREIGN KEY (id_produto) REFERENCES produto(id_produto) ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY (id_produto, id_estoque)
 );
